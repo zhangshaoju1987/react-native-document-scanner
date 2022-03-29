c635fa83a8f55a9e4eb430e4fb6a1d30df2f5b14
zhangshaoju1987 <zhangshaoju.12@gmail.com>
2022-03-29 22:39:58 +0800

    dasdasd

--- ios/RNPdfScannerManager.m
+++ ios/RNPdfScannerManager.m
@@ -81,7 +81,7 @@ RCT_EXPORT_METHOD(detectDocument:(NSString *)imageUri callback:(RCTResponseSende
                              } : [NSDictionary dictionary];
     
     NSDictionary * p = ciImage.properties;
-    callback(@[@{@"rectangleCoordinates":rectangleCoordinates,@"size":@{@"width":p[@"PixelWidth"],@"height":p[@"PixelHeight"]}}]);
+    callback(@[@{@"rectangleCoordinates":rectangleCoordinates,@"size":@{@"width":p[@"PixelHeight"],@"height":p[@"PixelWidth"]}}]);
 }
 
 RCT_EXPORT_METHOD(crop:(NSDictionary *)points imageUri:(NSString *)imageUri callback:(RCTResponseSenderBlock)callback)
